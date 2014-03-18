f2846d095bf1c0032202165879347290a7265731
wbenwick <wbenwick@gmail.com>
2014-03-18 09:33:27 -0400

    Initial Build

---
+++ DoIt/DoIt/FLXViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLXViewController.h
+//  DoIt
+//
+//  Created by Wes Benwick on 3/18/14.
+//  Copyright (c) 2014 FileLogix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLXViewController : UIViewController
+
+@end
---
+++ DoIt/DoIt/FLXViewController.m
@@ -0,0 +1,41 @@
+//
+//  FLXViewController.m
+//  DoIt
+//
+//  Created by Wes Benwick on 3/18/14.
+//  Copyright (c) 2014 FileLogix. All rights reserved.
+//
+
+#import "FLXViewController.h" <UITableViewDataSource>
+
+@interface FLXViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
+
+
+@end
+
+@implementation FLXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+@end
